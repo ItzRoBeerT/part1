c23ccef8e62c9af9f12468a5f8dd38edf81f7e5b
Roberto Caravaca <robertocaravaca436@gmail.com>
2025-08-29 17:50:29 +0200

    Step 2 - Course Information

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,22 @@ const Header = (props) => {
 	return <h1>{props.course}</h1>;
 };
 
-const Content = (props) => {
-	return props.parts.map((part) => (
-		<p key={part.id}>
-			{part.name} {part.exercises}
+const Part = (props) => {
+	return (
+		<p>
+			{props.name} {props.exercises}
 		</p>
-	));
+	);
+};
+
+const Content = (props) => {
+	return (
+		<div>
+			<Part name={props.parts[0].name} exercises={props.parts[0].exercises} />
+			<Part name={props.parts[1].name} exercises={props.parts[1].exercises} />
+			<Part name={props.parts[2].name} exercises={props.parts[2].exercises} />
+		</div>
+	);
 };
 
 const Total = (props) => {
