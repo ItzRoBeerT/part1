4fcdf9e53f401b78d8213a41376af4e9a303c647
Roberto Caravaca <robertocaravaca436@gmail.com>
2025-08-29 18:03:10 +0200

    Step 4 - Course Information

--- src/App.jsx
+++ src/App.jsx
@@ -26,32 +26,30 @@ const Total = (props) => {
 
 const App = () => {
 	const course = 'Half Stack application development';
-	const part1 = {
-		name: 'Fundamentals of React',
-		exercises: 10,
-	};
-	const part2 = {
-		name: 'Using props to pass data',
-		exercises: 7,
-	};
-	const part3 = {
-		name: 'State of a component',
-		exercises: 14,
-	};
+	const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
 	return (
 		<div>
 			<Header course={course} />
 
 			<Content
-				parts={[
-					{ id: 1, name: part1.name, exercises: part1.exercises },
-					{ id: 2, name: part2.name, exercises: part2.exercises },
-					{ id: 3, name: part3.name, exercises: part3.exercises },
-				]}
+				parts={parts}
 			/>
 
-			<Total total={part1.exercises + part2.exercises + part3.exercises} />
+			<Total total={parts[0].exercises + parts[1].exercises + parts[2].exercises} />
 		</div>
 	);
 };
